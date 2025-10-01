f907b8209a2ab15fd324c38eb560a935002e9722
yashashree-722 <yashashree@ratnamsolutions.com>
2025-10-01 18:45:42 +0530

    Update tenant_context_manage.py

--- isolated-schema/tenant_context_manage.py
+++ isolated-schema/tenant_context_manage.py
@@ -6,7 +6,7 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pollsapi.settings")
     try:
-        from django.core.management import execute_from_command_line
+        from django.core.management import executand_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
